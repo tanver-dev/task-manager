9998ba4fc3ec85e7ed6848350d8777693ee06fea
tanvir <tanvirkalim123@gmail.com>
2025-09-15 12:04:22 +0530

    update again authController.js

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -58,7 +58,12 @@ exports.loginUser = async (req, res) => {
 
 // Logout
 exports.logoutUser = (req, res) => {
-  res.cookie("token", null, { httpOnly: true, expires: new Date(0) });
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+    secure: true,
+    sameSite: "none",
+  });
   res.status(200).json({ message: "Logged out" });
 };
 
